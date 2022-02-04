c4ba031bdfa4d5dac61d8114521aa52a026cdf70
Brady Buchholz <bradybuchholz@gmail.com>
2022-02-03 22:43:27 -0500

    map shows up

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -2,9 +2,18 @@ const apiKey = 'AIzaSyDSprTRMx_B5u8ihxrZO3HDtopBjvAbOuQ'
 
 function getApi() {
   var queryURL = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap`; 
-  fetch(queryURL)
-      .then(function (response) {
-         console.log(response);
+  fetch(queryURL, { mode: 'no-cors'})
+      .then(function (res) {
+         console.log(res);
       })}
+
+      let map;
+
+      function initMap() {
+        map = new google.maps.Map(document.getElementById("map"), {
+          center: { lat: -34.397, lng: 150.644 },
+          zoom: 8,
+        });
+      }
 
 document.getElementById('button').addEventListener('click', getApi)
